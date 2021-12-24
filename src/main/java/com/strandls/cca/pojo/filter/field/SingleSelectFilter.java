96effb19b7c0cfe807d1cbaa9ff2faef66bb14c5
vilay <kandi.vilay09@gmail.com>
2021-12-24 12:23:56 +0530

    Fix : Single select is equivalent of multiselect in case of filter

--- src/main/java/com/strandls/cca/pojo/filter/field/SingleSelectFilter.java
+++ src/main/java/com/strandls/cca/pojo/filter/field/SingleSelectFilter.java
@@ -1,31 +1,5 @@
 package com.strandls.cca.pojo.filter.field;
 
-import org.bson.conversions.Bson;
-
-import com.mongodb.client.model.Filters;
-import com.strandls.cca.pojo.filter.Filter;
-
-public class SingleSelectFilter extends Filter {
-
-	private String value;
-
-	public String getValue() {
-		return value;
-	}
-
-	public void setValue(String value) {
-		this.value = value;
-	}
-	
-	@Override
-	public String getFieldHierarchy() {
-		return getCcaFieldValuesString() + "." + getFieldId() + ".value.value";
-	}
-
-	@Override
-	public Bson getFilter() {
-		String fieldHierarchy = getFieldHierarchy();
-		return Filters.in(fieldHierarchy, value);
-	}
+public class SingleSelectFilter extends MultiSelectFilter {
 
 }
