c1cdca6b0f920bdca5ce3442de6f816239cda55f
Guddu Kumar Sharma <guddusharma027@gmail.com>
2022-02-01 17:17:41 +0530

    removed unused import

--- src/main/java/com/strandls/cca/controller/CCATemplateController.java
+++ src/main/java/com/strandls/cca/controller/CCATemplateController.java
@@ -19,7 +19,6 @@ import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.core.Response.Status;
 
 import com.strandls.authentication_utility.filter.ValidateUser;
