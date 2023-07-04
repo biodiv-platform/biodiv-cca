bf23186bdf15d7f7ace6b32dac9c165c426890f9
abrahul <abrahul100@gmail.com>
2023-07-04 14:02:34 +0530

    added getAllCCAFieldIds,getChildFieldIds to get all cca field ids recursively from the data

--- src/main/java/com/strandls/cca/dao/CCATemplateDao.java
+++ src/main/java/com/strandls/cca/dao/CCATemplateDao.java
@@ -1,12 +1,16 @@
 package com.strandls.cca.dao;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import org.bson.Document;
 import org.bson.conversions.Bson;
 
 import com.google.inject.Inject;
+import com.mongodb.client.AggregateIterable;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Aggregates;
 import com.mongodb.client.model.Filters;
 import com.mongodb.client.model.Projections;
 import com.strandls.cca.CCAConstants;
@@ -61,6 +65,70 @@ public class CCATemplateDao extends AbstractDao<CCATemplate> {
 					.into(new ArrayList<CCATemplate>());
 		else
 			return dbCollection.find(bsonFilter).into(new ArrayList<CCATemplate>());
+	}
+
+	public List<String> getAllCCAFieldIds() {
+	    List<String> fieldIds = new ArrayList<>();
+	    try {
+	        // Connect to the database
+
+	        // Construct the aggregation pipeline
+	        List<Bson> pipeline = Arrays.asList(
+	            // Match the desired documents
+	            Aggregates.match(Filters.exists("fields.fieldId")),
+	            // Unwind the fields array to get individual field objects
+	            Aggregates.unwind("$fields"),
+	            // Group by the fieldId and add the fieldId to the group
+	            Aggregates.group("$fields.fieldId"),
+	            // Project the fieldId as output
+	            Aggregates.project(Projections.fields(Projections.excludeId(), Projections.computed("_id", "$_id.fieldId")))
+	        );
+
+	        // Execute the aggregation pipeline
+	        AggregateIterable<Document> result = dbCollection.aggregate(pipeline, Document.class);
+
+	        // Iterate over the aggregation result and add the fieldIds to the list
+	        for (Document document : result) {
+	            String fieldId = document.getString("_id");
+	            fieldIds.add(fieldId);
+	            System.out.println(fieldId);
+	        }
+
+	        // Get the children's field IDs recursively
+	        List<String> childFieldIds = getChildFieldIds(fieldIds);
+	        fieldIds.addAll(childFieldIds);
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	        // Handle the exception
+	    }
+	    return fieldIds;
+	}
+
+	public List<String> getChildFieldIds(List<String> parentFieldIds) {
+	    List<String> childFieldIds = new ArrayList<>();
+	    try {
+	        // Construct the aggregation pipeline to get children's field IDs
+	        List<Bson> pipeline = Arrays.asList(
+	            // Match the desired documents
+	            Aggregates.match(Filters.in("parentId", parentFieldIds)),
+	            // Group by the fieldId and add the fieldId to the group
+	            Aggregates.group("$fieldId")
+	        );
+
+	        // Execute the aggregation pipeline
+	        AggregateIterable<Document> result = dbCollection.aggregate(pipeline, Document.class);
+
+	        // Iterate over the aggregation result and add the fieldIds to the list
+	        for (Document document : result) {
+	            String fieldId = document.getString("_id");
+	            childFieldIds.add(fieldId);
+	            System.out.println(fieldId);
+	        }
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	        // Handle the exception
+	    }
+	    return childFieldIds;
 	}
 
 }
